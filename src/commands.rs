6b622de572b54416ac8bcd2a90e0e5a4828d477d
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-08-04 02:16:41 +0000

    Basic command types enum

---
+++ src/commands.rs
@@ -0,0 +1,24 @@
+pub enum Commands {
+    Add,
+    Reset,
+    Commit,
+}
+
+impl Commands {
+    pub fn get_from_string(input: &String) -> Option<Self> {
+        match input.to_lowercase().as_str() {
+            "add" => Some(Commands::Add),
+            "reset" => Some(Commands::Reset),
+            "commit" => Some(Commands::Commit),
+            _ => None,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match self {
+            Commands::Add => String::from("add"),
+            Commands::Reset => String::from("reset"),
+            Commands::Commit => String::from("commit"),
+        }
+    }
+}
