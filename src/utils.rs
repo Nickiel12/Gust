378b25556b34c5bf8724f8417f6cf96f5285bf5a
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2022-08-19 19:27:50 -0700

    adds function to clear colors from a vec of strings

--- src/utils.rs
+++ src/utils.rs
@@ -3,3 +3,14 @@ use console;
 pub fn strip_colors(input: String) -> String {
     return console::strip_ansi_codes(input.as_str()).to_string();
 }
+
+pub fn strip_vec_colors(mut input: Vec<String>) -> Vec<String> {
+    // Pretty cool piece of in place filtering
+    // for evering mutable reference to the items in input
+    // set the value behind the reference (how I think the * works)
+    // to equal that value put through the function 'strip_colors'
+    for i in &mut input {
+        *i = strip_colors((*i).to_string());
+    }
+    return input;
+}
