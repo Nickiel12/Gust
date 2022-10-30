1b491bf8f33d497346d0c48fe30cdf44fc3f0c7d
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-10-29 19:52:15 -0700

    Trimmed up the shell.nix file

--- shell.nix
+++ shell.nix
@@ -1,21 +1,17 @@
+# <shell.nix>
 { pkgs ? import <nixpkgs> {}}:
-
-let
-  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  ruststable = (pkgs.latest.rustChannels.stable.default.override {
-      extensions = [
-        "rust-src"
-        "rustfmt"
-        "clippy"
-      ];
+ 
+ let
+   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+   ruststable = (pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+        ];
     });
-in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    ruststable
-  ];
-
-  RUST_BACKTRACE = 1;
-}
-
+ in
+ pkgs.mkShell {
+   buildInputs = with pkgs; [
+     ruststable
+   ];
+ }
